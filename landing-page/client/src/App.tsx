a5e0e011248fedc4bfeaf053e54fc573a8df6386
GroundTruthIndex26 <2.72652994e+08+GroundTruthIndex26@users.noreply.github.com>
2026-08-27 20:26:14 +0000

    Fix GitHub Pages preview base routing

--- landing-page/client/src/App.tsx
+++ landing-page/client/src/App.tsx
@@ -5,12 +5,14 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
+import { Route, Router as WouterRouter, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 
-function Router() {
+const routerBase = import.meta.env.BASE_URL === "/" ? undefined : import.meta.env.BASE_URL.replace(/\/$/, "");
+
+function AppRoutes() {
   if (window.location.protocol === "file:") {
     return <Home />;
   }
@@ -36,7 +38,9 @@ function App() {
       <ThemeProvider defaultTheme="light">
         <TooltipProvider>
           <Toaster position="bottom-right" richColors />
-          <Router />
+          <WouterRouter base={routerBase}>
+            <AppRoutes />
+          </WouterRouter>
         </TooltipProvider>
       </ThemeProvider>
     </ErrorBoundary>
